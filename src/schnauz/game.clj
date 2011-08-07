6b6eb3834dee70332cf856badb985942617393be
Benjamin Klüglein <scheibenkaes@googlemail.com>
2011-08-08 00:46:50 +0200

    * ignoring *.swp * small refactoring

--- src/schnauz/game.clj
+++ src/schnauz/game.clj
@@ -2,11 +2,24 @@
   (:use clojure.contrib.combinatorics))
 
 (def colors 
-  [:clubs :diamonds :hearts :spades])
+  #{:clubs :diamonds :hearts :spades})
 
 (def symbols 
-  [7 8 9 10 :jack :queen :king :ace])
+  #{7 8 9 10 :jack :queen :king :ace})
+
+(defn card [col value] 
+  (assert (contains? colors col))
+  (assert (contains? symbols value))
+  {:color col :symbol value})
+
+(defn value [{sym :symbol}]
+  (if (number? sym)
+    sym
+    (cond 
+      (contains? #{:jack :queen :king} sym) 10
+      (= sym :ace) 11)))
 
 (def deck 
   (cartesian-product colors symbols))
+
 
--- test/schnauz/test/game.clj
+++ test/schnauz/test/game.clj
@@ -1,1 +1,17 @@
-(ns schnauz.test.game)
+(ns schnauz.test.game
+  (:use schnauz.game)
+  (:use clojure.test))
+
+(deftest deck-consits-of-32-cards 
+  (is (= 32 (count deck))))
+
+(deftest test-value-of-cards 
+  (are [x y] (= x y)
+       11 (value (card :hearts :ace))
+       10 (value (card :hearts :king))
+       10 (value (card :hearts :queen))
+       10 (value (card :hearts :jack))
+       10 (value (card :hearts 10))
+       9 (value (card :hearts 9))
+       8 (value (card :hearts 8))
+       7 (value (card :hearts 7))))
