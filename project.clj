bd87a7f3b7411f6ddb94f3c6a406ef9c2b896872
Benjamin Klüglein <scheibenkaes@googlemail.com>
2011-08-09 23:50:16 +0200

    Removed lazytest

--- project.clj
+++ project.clj
@@ -1,8 +1,6 @@
 (defproject schnauz "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :repositories {"stuartsierra" "http://stuartsierra.com/maven2"}
   :dev-dependencies [[org.clojars.autre/lein-vimclojure "1.0.0"]
                      [lein-vim "1.0.1-SNAPSHOT"]]
   :dependencies [[org.clojure/clojure "1.2.1"]
-                 [org.clojure/clojure-contrib "1.2.0"]
-                 [com.stuartsierra/lazytest "1.2.3"]])
+                 [org.clojure/clojure-contrib "1.2.0"]])
